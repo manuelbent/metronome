f5d7971d742852910f397b52c5e2276d307df3e2
manuelbent <manuel.bentivoglio@ef.com>
2023-07-25 14:39:29 +0200

    added pendulum animation

--- metronome.js
+++ metronome.js
@@ -1,18 +1,31 @@
-(async () => {
-  // define the beats per minute
+import { left, right, display } from './pendulum.js'
+
+;(async () => {
+  // clear the whole console
+  console.clear()
+
+  // set the beats per minute
   const bpm = +process.argv[2] || 60
 
-  // defining the interval to wait in between beats
-  const timeout = 60 / bpm * 1000
-
-  // set the interval time between beats
+  // define the interval to wait for in between beats
   const interval = () => {
-    return new Promise(resolve => setTimeout(resolve, timeout))
+    return new Promise(resolve => setTimeout(resolve, 60 / bpm * 1000))
   }
 
   // start the metronome
-  while (1) {
+  for (let i = 1; true;) {
+    // render the pendulum
+    console.log(++i % 2 === 0 ? right : left)
+
+    // render the bpm display
+    console.log(display.replace('{{}}', String(bpm)))
+
+    // play the beat
     process.stdout.write('\x07')
+
+    // wait for the interval
     await interval()
+
+    console.clear()
   }
 })()
