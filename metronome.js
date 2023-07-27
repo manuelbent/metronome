c04a054f04c2bc52a1b3d1fedec45a97a694e63d
manuelbent <manuel.bentivoglio@ef.com>
2023-07-27 11:59:42 +0200

    added check

--- metronome.js
+++ metronome.js
@@ -1,11 +1,15 @@
 import { left, right, display } from './pendulum.js'
 
 ;(async () => {
+  // get the bpm and force it to positive, use 60 as default
+  const bpm = Math.abs(+process.argv[2] || 60)
+  if ((bpm - 1)*(bpm - 240) > 0) {
+    console.log('Please use a value between 1 and 240 (or let it default to 60).')
+    return
+  }
+
   // clear the whole console
   console.clear()
-
-  // set the beats per minute
-  const bpm = +process.argv[2] || 60
 
   // define the interval to wait for in between beats
   const interval = () => {
